7accf6af314d12d15860231c8bb53a2944ccda98
seaios <nradman@gmail.com>
2017-03-08 16:44:56 -0800

    load SingleGamePassViewController when selecting appropriate ticket cell in PassTypeVC

--- iOSTemplate/SingleGamePassViewController.swift
+++ iOSTemplate/SingleGamePassViewController.swift
@@ -1,93 +1,71 @@
 //
-//  PassTypeViewController.swift
+//  SingleGamePassViewController.swift
 //  iOSTemplate
 //
-//  Created by Nichole Radman on 3/6/17.
+//  Created by Nichole Radman on 3/7/17.
 //  Copyright © 2017 Sportsrocket. All rights reserved.
 //
 
 import UIKit
 
-class PassTypeViewController: UITableViewController {
+class SingleGamePassViewController: UITableViewController {
 
   override func viewDidLoad() {
-    tableView.separatorColor = UIColor.clear
-    tableView.backgroundColor = UIColor.black
+    tableView.separatorColor = UIColor.black
+
     super.viewDidLoad()
 
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
+      // Uncomment the following line to preserve selection between presentations
+      // self.clearsSelectionOnViewWillAppear = false
 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+      // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+      // self.navigationItem.rightBarButtonItem = self.editButtonItem()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+      // Dispose of any resources that can be recreated.
   }
 
   // MARK: - Table view data source
 
   override func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
+      // #warning Incomplete implementation, return the number of sections
+    // TODO: number from API
+    return 2
   }
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // TODO: 3 through burger menu, 4 through playback
+      // #warning Incomplete implementation, return the number of rows
     return 3
-  }
-
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    tableView.register(UINib(nibName: "TicketTableViewCell", bundle: nil), forCellReuseIdentifier: "ticketCell")
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ticketCell", for: indexPath) as! TicketTableViewCell
-    cell.backgroundColor = .black
-    // TODO: remove hardcoded and add from SDK
-    switch indexPath.row {
-    case 0:
-      cell.passLeadingImage.image = UIImage(named: "ticketTailSingle")
-      cell.passTrailingImage.image = UIImage(named: "singleButton")
-    case 1:
-      cell.passLeadingImage.image = UIImage(named: "ticketTailSeason")
-      cell.passTrailingImage.image = UIImage(named: "seasonButton")
-      cell.passTopBorderView.backgroundColor = UIColor(red: 92/255, green: 19/255, blue: 20/255, alpha: 1.0)
-      cell.passBottomBorderView.backgroundColor = UIColor(red: 92/255, green: 19/255, blue: 20/255, alpha: 1.0)
-      cell.passTitle.text = "NLL TV Season Pass"
-      cell.passSubtitle.text = "2017 League Season Pass"
-    case 2:
-      cell.passLeadingImage.image = UIImage(named: "ticketTailTeam")
-      cell.passTrailingImage.image = UIImage(named: "teamButton")
-      cell.passTopBorderView.backgroundColor = UIColor(red: 69/255, green: 93/255, blue: 113/255, alpha: 1.0)
-      cell.passBottomBorderView.backgroundColor = UIColor(red: 69/255, green: 93/255, blue: 113/255, alpha: 1.0)
-      cell.passTitle.text = "Team Season Pass"
-      cell.passSubtitle.text = "Team Season Pass"
-      cell.passPrice.text = "$29.99"
-    default:
-      break
-    }
-    return cell
   }
   
   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return 128.0
   }
-  
-  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return "Is there a header or not here?"
-  }
-  
-  override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-    return "Legal stuff, blackout info, etc."
+
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    tableView.register(UINib(nibName: "TicketTableViewCell", bundle: nil), forCellReuseIdentifier: "ticketCell")
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ticketCell", for: indexPath) as! TicketTableViewCell
+
+    return cell
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    switch indexPath.row {
-    case 0:
-      let viewController = SingleGamePassViewController()
-      self.navigationController?.pushViewController(viewController, animated: true)
-    default:
-      break
-    }
+    // TODO: check if logged in
+    let viewController = PurchaseConfirmViewController()
+    self.navigationController?.pushViewController(viewController, animated: true)
+    // TODO: remove hardcoded
+    viewController.view.backgroundColor = UIColor(red: 167/255, green: 147/255, blue: 25/255, alpha: 1.0)
+    viewController.title = "Purchase Confirmation"
+    viewController.passTitle.text = "Toronto vs Vancouver".uppercased()
+    viewController.passSubtitle.text = "Jan 5th, 2017 @ 5:00 PM"
+  }
+  
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    // TODO: need dates
+    return "January 5th, 2017"
   }
 
     /*
