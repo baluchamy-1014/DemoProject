c7d1465c92687f8e55560f3bc1bcb0ecdd2dad75
seaios <nradman@gmail.com>
2017-03-15 13:59:20 -0700

    removal of auto-generated commented out code in new view controllers

--- iOSTemplate/PurchaseConfirmViewController.swift
+++ iOSTemplate/PurchaseConfirmViewController.swift
@@ -21,28 +21,14 @@ class PurchaseConfirmViewController: UIViewController {
     self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(PurchaseConfirmViewController.cancelPurchase))
 
     super.viewDidLoad()
-    
-    // Do any additional setup after loading the view.
   }
   
   override func didReceiveMemoryWarning() {
       super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
   }
   
   func cancelPurchase() {
     _ = self.navigationController?.popToRootViewController(animated: true)
   }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
