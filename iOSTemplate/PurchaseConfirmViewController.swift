5cc5c9652ee06e926afd127da2cae48b2de92b22
seaios <nradman@gmail.com>
2017-03-21 17:16:43 -0700

    update confirm screen to match new specs

--- iOSTemplate/PurchaseConfirmViewController.swift
+++ iOSTemplate/PurchaseConfirmViewController.swift
@@ -13,10 +13,15 @@ class PurchaseConfirmViewController: UIViewController {
   @IBOutlet var passTitle: UILabel!
   @IBOutlet var passSubtitle: UILabel!
   @IBOutlet var promoApplyButton: UIButton!
+  @IBOutlet var promoTextField: UITextField!
   
   override func viewDidLoad() {
-    promoApplyButton.layer.borderColor = UIColor.lightGray.cgColor
+    promoApplyButton.layer.borderColor = UIColor(red: 151/255, green: 151/255, blue: 151/255, alpha: 1.0).cgColor
     promoApplyButton.layer.borderWidth = 1.0
+    promoApplyButton.layer.cornerRadius = 4.0
+    
+    promoTextField.layer.borderColor = UIColor(red: 151/255, green: 151/255, blue: 151/255, alpha: 1.0).cgColor
+    promoTextField.layer.borderWidth = 1.0
     
     self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(PurchaseConfirmViewController.cancelPurchase))
 
