d83ccf7b7fc04fea907ff0b01a00468ec973ee7f
Baluchamy, ANGLER - EIT <baluchamy@ang142.local>
2017-03-31 13:11:27 +0530

    Sample Xib added

---
+++ DemoProject/SampleAdded.swift
@@ -0,0 +1,18 @@
+//
+//  SampleAdded.swift
+//  DemoProject
+//
+//  Created by Baluchamy, ANGLER - EIT on 3/31/17.
+//  Copyright © 2017 Baluchamy, ANGLER - EIT. All rights reserved.
+//
+
+import Cocoa
+
+class SampleAdded: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
