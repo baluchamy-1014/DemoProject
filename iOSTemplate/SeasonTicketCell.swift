d4551617a6bcae3d8b76616061751fca68ea42fc
seaios <nradman@gmail.com>
2017-05-08 18:00:56 -0700

    AAPL-386 design setup for Season Ticket Cell

---
+++ iOSTemplate/SeasonTicketCell.swift
@@ -0,0 +1,24 @@
+//
+//  SeasonTicketCell.swift
+//  iOSTemplate
+//
+//  Created by Nichole Radman on 5/8/17.
+//  Copyright © 2017 Sportsrocket. All rights reserved.
+//
+
+import UIKit
+
+class SeasonTicketCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
