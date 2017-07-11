de6384765770f3c8b15e0ad8c30f1ec0a61d44f1
baluchamy-1014 <baluchamy@angleritech.com>
2017-07-11 11:01:40 +0530

    Example Resource

---
+++ DemoProject/Resources/Resource.swift
@@ -0,0 +1,13 @@
+//
+//  Resource.swift
+//  DemoProject
+//
+//  Created by Baluchamy, ANGLER - EIT on 7/11/17.
+//  Copyright © 2017 Baluchamy, ANGLER - EIT. All rights reserved.
+//
+
+import Cocoa
+
+class Resource: NSObject {
+
+}
