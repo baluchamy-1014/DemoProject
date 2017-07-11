a1a7d025c08906b025a3450b5eff01a34ff78622
baluchamy-1014 <baluchamy@angleritech.com>
2017-07-11 11:09:42 +0530

    Example Artifact Added

---
+++ DemoProject/Resources/Artifact.swift
@@ -0,0 +1,13 @@
+//
+//  Artifact.swift
+//  DemoProject
+//
+//  Created by Baluchamy, ANGLER - EIT on 7/11/17.
+//  Copyright © 2017 Baluchamy, ANGLER - EIT. All rights reserved.
+//
+
+import Cocoa
+
+class Artifact: NSObject {
+
+}
