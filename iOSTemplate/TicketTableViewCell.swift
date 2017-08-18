7a0f99364c6cb39a88406e54e3fd4de630d6b995
seaios <nradman@gmail.com>
2017-08-18 16:53:27 -0700

    AAPL-476 adjust season and pass cells to only display 76 percent width on iPads

--- iOSTemplate/TicketTableViewCell.swift
+++ iOSTemplate/TicketTableViewCell.swift
@@ -28,5 +28,26 @@ class TicketTableViewCell: UITableViewCell {
 
       // Configure the view for the selected state
   }
+  
+  override var frame: CGRect {
+    get {
+      return super.frame
+    }
+    set (newFrame) {
     
+      var frame = newFrame
+      var newWidth = frame.width
+      
+      if DeviceChecker.DeviceType.IS_IPAD || DeviceChecker.DeviceType.IS_IPAD_PRO {
+        newWidth = frame.width * 0.76
+      }
+      let margins = (frame.width - newWidth) / 2
+      frame.size.width = newWidth
+      frame.origin.x += margins
+      
+      super.frame = frame
+      
+    }
+  }
+  
 }
