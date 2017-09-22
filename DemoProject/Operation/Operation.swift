45cf943586661cd82ab5ae5a721cc7c82d07f457
baluchamy-1014 <baluchamy@angleritech.com>
2017-09-22 10:24:08 +0530

    Operation Added

---
+++ DemoProject/Operation/Operation.swift
@@ -0,0 +1,13 @@
+//
+//  Operation.swift
+//  DemoProject
+//
+//  Created by Baluchamy, ANGLER - EIT on 9/22/17.
+//  Copyright © 2017 Baluchamy, ANGLER - EIT. All rights reserved.
+//
+
+import Cocoa
+
+class Operation: NSObject {
+
+}
