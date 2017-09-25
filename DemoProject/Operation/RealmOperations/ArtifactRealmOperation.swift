6f3ebe4a8457b405a5aef27e00f3052d8afde5ba
baluchamy-1014 <baluchamy@angleritech.com>
2017-09-25 11:38:30 +0530

    Artifact realm operation added

---
+++ DemoProject/Operation/RealmOperations/ArtifactRealmOperation.swift
@@ -0,0 +1,13 @@
+//
+//  ArtifactRealmOperation.swift
+//  DemoProject
+//
+//  Created by Baluchamy, ANGLER - EIT on 9/25/17.
+//  Copyright © 2017 Baluchamy, ANGLER - EIT. All rights reserved.
+//
+
+import Cocoa
+
+class ArtifactRealmOperation: NSObject {
+
+}
